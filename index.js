854762e2172c6182d45095367774a2b4c6d3a35b
Samantha Jordan <shinyguns@gmail.com>
2019-12-27 12:59:51 -0600

    first update with comments

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+//two
+let emptyArray = [];
+
+//one
+function theBeatlesPlay(musicians, instruments) {
+
+//three
+  function forLoop() {
+    for (var i = 0; i < musicians.length; i++){
+
+//four & five
+      emptyArray.push(`${musicians} plays ${instruments}`);
+    }
+
+//six
+    return emptyArray;
+  }
+}
+
+//seven & eight
+function johnLennonFacts(factsArray) {
+  
+}
+
+//one
+function iLoveTheBeatles(int) {
+  
+}// add solution here
